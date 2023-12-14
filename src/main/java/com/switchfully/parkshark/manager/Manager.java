c2c084e4ad287b789abdead6d8c5478bbfb3547c
Manon Locht <mlocht@outlook.com>
2023-12-14 13:32:21 +0100

    update pom.xml & add manager class

---
+++ src/main/java/com/switchfully/parkshark/manager/Manager.java
@@ -0,0 +1,27 @@
+package src.main.java.com.switchfully.parkshark.manager;
+
+@Entity
+public class Manager {
+
+    private final int id;
+    private final String username;
+    private final String password;
+
+    public Manager(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
