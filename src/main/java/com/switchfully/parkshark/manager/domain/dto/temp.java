17bb55749b06ef1a96f54ae1830a609d39e8c12d
web-Nuo <mlocht@outlook.com>
2023-12-15 13:24:35 +0100

    delete ManagerDTO

---
+++ src/main/java/com/switchfully/parkshark/manager/domain/dto/temp.java
@@ -0,0 +1,32 @@
+package com.switchfully.parkshark.manager.domain.dto;
+
+public class temp {
+
+
+//    package com.switchfully.parkshark.manager.domain.dto;
+//
+//    public class ManagerDto {
+//        private long id = 1L;
+//        private final String username;
+//        private final String password;
+//
+//        public ManagerDto(long id, String username, String password) {
+//            this.id = id;
+//            this.username = username;
+//            this.password = password;
+//        }
+//
+//        public long getId() {
+//            return id;
+//        }
+//
+//        public String getUsername() {
+//            return username;
+//        }
+//
+//        public String getPassword() {
+//            return password;
+//        }
+//    }
+
+}
