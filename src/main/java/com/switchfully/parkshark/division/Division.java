2a7736a0f961915242e73838692b745e834f7a23
Balder Bongaerts <bbongaerts@gmail.com>
2023-12-18 14:06:06 +0100

    add Division entity

---
+++ src/main/java/com/switchfully/parkshark/division/Division.java
@@ -0,0 +1,69 @@
+package com.switchfully.parkshark.division;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "DIVISION")
+public class Division {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private long id;
+   
+   @Column(name = "division_name")
+   private String divisionName;
+   
+   @Column(name = "original_name")
+   private String originalName;
+   
+   @Column(name = "director")
+   private String director;
+   
+   public Division() {
+   }
+   
+   public Division(long id, String divisionName, String originalName, String director) {
+      this.id = id;
+      this.divisionName = divisionName;
+      this.originalName = originalName;
+      this.director = director;
+   }
+   
+   public long getId() {
+      return id;
+   }
+   
+   public String getDivisionName() {
+      return divisionName;
+   }
+   
+   public String getOriginalName() {
+      return originalName;
+   }
+   
+   public String getDirector() {
+      return director;
+   }
+   
+   @Override
+   public String toString() {
+      return "Division{" + "id=" + id + ", divisionName='" + divisionName + '\'' + ", originalName='" + originalName + '\'' + ", director='" + director + '\'' + '}';
+   }
+   
+   @Override
+   public boolean equals(Object o) {
+      if (this == o)
+         return true;
+      if (o == null || getClass() != o.getClass())
+         return false;
+      Division division = (Division) o;
+      return id == division.id;
+   }
+   
+   @Override
+   public int hashCode() {
+      return Objects.hash(id);
+   }
+}
