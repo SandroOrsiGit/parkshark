f1ac2113fb8a2baf698071fecf0b3deedc61d1d5
Balder Bongaerts <bbongaerts@gmail.com>
2023-12-18 15:14:32 +0100

    refactor: cleanup code

--- src/main/java/com/switchfully/parkshark/division/domain/dto/DivisionDto.java
+++ src/main/java/com/switchfully/parkshark/division/domain/dto/DivisionDto.java
@@ -2,25 +2,17 @@ package com.switchfully.parkshark.division.domain.dto;
 
 public class DivisionDto {
    
-   private final long id;
-   
-   public DivisionDto(long id, String divisionName, String originalName, String director) {
-      this.id = id;
-      this.divisionName = divisionName;
-      this.originalName = originalName;
-      this.director = director;
-   }
-   
    private final String divisionName;
    
    private final String originalName;
    
    private final String director;
    
-  
-   
-   public long getId() {
-      return id;
+   public DivisionDto(String divisionName, String originalName, String director) {
+      
+      this.divisionName = divisionName;
+      this.originalName = originalName;
+      this.director = director;
    }
    
    public String getDivisionName() {
